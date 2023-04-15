9ab677bdc93101f264d4a5aefd59cea5fda13e6a
Kristina Morozova <christina.vadi@gmail.com>
2023-04-15 16:08:41 +0600

    feat: stack implementation via array

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,11 +1,12 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 class BinarySearchTree {
 
   root() {
